961d370056e583e5772b7b62ebcfbb9757705130
Adnan Karsic <adokce@gmail.com>
2021-12-12 21:44:22 +0100

    Introduce WebClient (still needs to be abstracted in proper service)

--- src/main/java/com/sloweth/backend/test/TestController.java
+++ src/main/java/com/sloweth/backend/test/TestController.java
@@ -1,12 +1,26 @@
 package com.sloweth.backend.test;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @RestController
 public class TestController {
+
+    @Autowired
+    private WebClient.Builder webClientBuilder;
+
     @GetMapping("/test")
-    public Test test(){
-        return new Test(1,"Test Content");
-    };
+    public Mono<String> test(){
+
+        String tempUri = "/api?module=account&action=txlist&address=0xddbd2b932c763ba5b1b7ae3b362eac3e8d40121a&startblock=0&endblock=99999999&page=1&offset=10&sort=asc&";
+
+        return webClientBuilder.baseUrl("https://api.etherscan.io").build()
+                .get()
+                .uri(tempUri)
+                .retrieve().bodyToMono(String.class);
+    }
 }
+
